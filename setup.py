bc551b3bc747019d1ef01d85d5bffdc50cfbe9d3
othalla <othalla.lf@gmail.com>
2019-01-11 11:25:21 +0100

    Module bootstrap

---
+++ setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+version = "0.0.1"
+
+
+setuptools.setup(
+    name="puppetdb_exporter",
+    version=version,
+    description="Prometheus puppetdb exporter",
+    author="Florian Chardin",
+    author_email="othalla.lf@gmail.com",
+    url="https://github.com/othalla/puppetdb_exporter",
+    long_description=open('README.rst').read(),
+    packages=setuptools.find_packages(exclude=['tests*']),
+    package_data={},
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development",
+    ],
+    install_requires=[
+        "pypuppetdb==0.3.3"
+    ],
+    extras_require={},
+    scripts=['scripts/type_checker.sh'],
+)
