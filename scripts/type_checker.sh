48c1bf33ce5cefc124689ac590281818d13dad7b
othalla <othalla.lf@gmail.com>
2019-01-12 12:57:21 +0100

    mypy allow untyped decorator for hug compat

--- scripts/type_checker.sh
+++ scripts/type_checker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -e
-find puppetdb_exporter -name "*.py" | xargs mypy --ignore-missing-imports --strict --disallow-untyped-defs --disallow-untyped-calls --disallow-incomplete-defs
+find puppetdb_exporter -name "*.py" | xargs mypy --ignore-missing-imports --strict --disallow-untyped-defs --disallow-untyped-calls --disallow-incomplete-defs --allow-untyped-decorators
