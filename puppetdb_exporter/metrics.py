a948b9410e045df1437e12e07eb382f563b3faa2
othalla <othalla.lf@gmail.com>
2019-01-25 10:03:31 +0100

    Set puppetdb_nodes_status gauge

--- puppetdb_exporter/metrics.py
+++ puppetdb_exporter/metrics.py
@@ -7,6 +7,7 @@ from puppetdb_exporter.puppetdb import get_nodes
 
 
 GAUGE_NODES = Gauge('puppetdb_nodes_registered', 'Description of gauge')
+GAUGE_STATUS = Gauge('puppetdb_nodes_status', 'desc', labelnames=['status'])
 
 
 class MetricsRender(Thread):
@@ -20,3 +21,15 @@ class MetricsRender(Thread):
     def _generate_metrics(self) -> None:
         nodes = self._node_provider()
         GAUGE_NODES.set(len(nodes))
+        status_values = {
+            'changed': 0,
+            'failed': 0,
+            'noop': 0,
+            'skipped': 0,
+            'unchanged': 0,
+            'unreported': 0,
+        }
+        for node in nodes:
+            status_values[node.status] += 1
+        for status_value in status_values:
+            GAUGE_STATUS.labels(status=status_value).set(status_values[status_value])
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -7,8 +7,25 @@ from puppetdb_exporter.metrics import MetricsRender
 class TestMetricsRender:
     @staticmethod
     def test_it_set_the_nodes_registered_gauge_metric():
+        node1 = MagicMock(name='node1', status='unchanged')
+        node2 = MagicMock(name='node2', status='changed')
+        node3 = MagicMock(name='node3', status='changed')
         get_nodes = MagicMock(name='get_config',
-                              return_value=['node1', 'node2'])
+                              return_value=[node1, node2, node3])
         metrics = MetricsRender(get_nodes)
         metrics.run()
-        assert REGISTRY.get_sample_value('puppetdb_nodes_registered') == 2
+        assert REGISTRY.get_sample_value('puppetdb_nodes_registered') == 3
+
+    @staticmethod
+    def test_it_set_the_nodes_by_status_gauge_metric():
+        node1 = MagicMock(name='node1', status='unchanged')
+        node2 = MagicMock(name='node2', status='changed')
+        node3 = MagicMock(name='node3', status='changed')
+        get_nodes = MagicMock(name='get_config',
+                              return_value=[node1, node2, node3])
+        metrics = MetricsRender(get_nodes)
+        metrics.run()
+        assert REGISTRY.get_sample_value('puppetdb_nodes_status',
+                                         {'status': 'changed'}) == 2
+        assert REGISTRY.get_sample_value('puppetdb_nodes_status',
+                                         {'status': 'unchanged'}) == 1
