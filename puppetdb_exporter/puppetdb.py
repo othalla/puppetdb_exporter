7995c0cca2bc18a22eec6a95bc9e8b82936d4278
othalla <othalla.lf@gmail.com>
2019-01-28 08:32:50 +0100

    Simplify puppetdb module

--- puppetdb_exporter/puppetdb.py
+++ puppetdb_exporter/puppetdb.py
@@ -2,23 +2,18 @@ from pypuppetdb import connect
 
 from puppetdb_exporter.config import get_config
 
-PUPPETDB_CONNEXION = None
-
 
 def _get_puppetdb_connexion():
-    global PUPPETDB_CONNEXION
-    if PUPPETDB_CONNEXION is None:
-        config = get_config()
-        PUPPETDB_CONNEXION = connect(host=config['PUPPETDB_HOST'],
-                                     port=config['PUPPETDB_PORT'],
-                                     ssl_verify=config['PUPPETDB_SSL_VERIFY'],
-                                     ssl_key=config['PUPPETDB_KEY'],
-                                     ssl_cert=config['PUPPETDB_CERT'],
-                                     timeout=config['PUPPETDB_TIMEOUT'],
-                                     protocol=config['PUPPETDB_PROTO'],)
-    return PUPPETDB_CONNEXION
+    config = get_config()
+    return connect(host=config['PUPPETDB_HOST'],
+                   port=config['PUPPETDB_PORT'],
+                   ssl_verify=config['PUPPETDB_SSL_VERIFY'],
+                   ssl_key=config['PUPPETDB_KEY'],
+                   ssl_cert=config['PUPPETDB_CERT'],
+                   timeout=config['PUPPETDB_TIMEOUT'],
+                   protocol=config['PUPPETDB_PROTO'],)
 
 
 def get_nodes():
-    database = PUPPETDB_CONNEXION
+    database = _get_puppetdb_connexion()
     return database.nodes()
