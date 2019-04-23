9796782173c9baefb8b72fedfae543e4358d5931
othalla <othalla.lf@gmail.com>
2019-04-23 13:16:23 +0200

    Reword config module

--- puppetdb_exporter/config.py
+++ puppetdb_exporter/config.py
@@ -1,41 +1,31 @@
 from configparser import ConfigParser
 from os import environ
-from typing import List, Type
 
-SETTINGS = [
-    'puppetdb_host',
-    'puppetdb_port',
-    'puppetdb_ssl_verify',
-    'puppetdb_key',
-    'puppetdb_cert',
-    'puppetdb_proto',
-]
+CONFIG_FILE_ERROR_MESSAGE = ('No configuration file provided. '
+                             'You must set CONFIG_FILE environment variable.')
 
 
 class ConfigurationException(Exception):
     pass
 
 
-def get_config(
-        config_parser: Type[ConfigParser] = ConfigParser) -> ConfigParser:
-    configuration = config_parser()
-    configuration.read(_read_config_files())
-    _check_config_settings(configuration)
-    return configuration
+class Configuration():
+    def __init__(self) -> None:
+        self._configuration = ConfigParser()
+        if 'CONFIG_FILE' not in environ or not environ['CONFIG_FILE']:
+            raise ConfigurationException('No configuration file provided. '
+                                         'You must set CONFIG_FILE '
+                                         'environment variable.')
+        if not self._configuration.read(environ['CONFIG_FILE']):
+            raise ConfigurationException('Failed to load configuration file. '
+                                         'You must provide a valide file '
+                                         'in the CONFIG_FILE '
+                                         'environment variale.')
 
+    @property
+    def puppetdb_host(self) -> str:
+        return self._configuration.get('puppetdb', 'host')
 
-def _read_config_files() -> List[str]:
-    if 'CONFIG_FILE' in environ:
-        return [environ['CONFIG_FILE']]
-    return ['/etc/puppetdb_exporter/config.ini',
-            '.config/puppetdb_exporter/config.ini',
-            'config.ini']
-
-
-def _check_config_settings(configuration: ConfigParser) -> None:
-    if not configuration.has_section('main'):
-        raise ConfigurationException('Missing main section in config file.')
-    for setting in SETTINGS:
-        if not configuration.has_option('main', setting):
-            raise ConfigurationException(f'Missing {setting} setting in '
-                                         'config file.')
+    @property
+    def puppetdb_port(self) -> str:
+        return self._configuration.get('puppetdb', 'port')
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,52 +1,39 @@
-from configparser import ConfigParser
-from unittest.mock import create_autospec, patch
+from os import environ, path
+from unittest.mock import patch
+from pathlib import Path
 
 import pytest
 
-from puppetdb_exporter.config import (SETTINGS, ConfigurationException,
-                                      get_config)
+from puppetdb_exporter.config import (ConfigurationException, Configuration)
+
+FIXTURE_CONFIG = path.join(Path(__file__).absolute().parent,
+                           'fixtures',
+                           'config.ini')
 
 
-class TestConfig:
+class TestConfiguration:
     @staticmethod
-    def test_it_load_given_config_file_from_environment_variable():
-        config_parser = create_autospec(ConfigParser)
-        with patch.dict('os.environ', {'CONFIG_FILE': '/path/config'}):
-            get_config(config_parser=config_parser)
-            config_parser.return_value.read.assert_called_once_with(
-                ['/path/config'])
-
-    @staticmethod
-    def test_it_load_config_file_from_default_files():
-        config_parser = create_autospec(ConfigParser)
-        get_config(config_parser=config_parser)
-        config_parser.return_value.read.assert_called_once_with(
-            ['/etc/puppetdb_exporter/config.ini',
-             '.config/puppetdb_exporter/config.ini',
-             'config.ini']
-        )
-
-    @staticmethod
-    def test_it_cannot_load_configuration_without_main_section():
-        with patch.dict('os.environ', {'CONFIG_FILE': '/path/config/ko'}):
-            with pytest.raises(ConfigurationException,
-                               match='Missing main section in config file.'):
-                get_config()
-
-    @staticmethod
-    def test_it_cannot_load_configuration_without_puppetdb_settings():
-        config_parser = create_autospec(ConfigParser)
-        config_parser.return_value.has_option.return_value = False
+    def test_with_no_config_file_environment_variable():
         with pytest.raises(ConfigurationException,
-                           match=r'Missing .* setting in config file.'):
-            get_config(config_parser=config_parser)
+                           match=r'No configuration file provided.*'):
+            _ = Configuration()
 
     @staticmethod
-    def test_it_load_configuration_properly():
-        config_parser = create_autospec(ConfigParser)
-        config_parser.return_value.has_option.return_value = True
-        get_config(config_parser=config_parser)
-        assert config_parser.return_value.has_option.call_count == len(SETTINGS)
-        for setting in SETTINGS:
-            config_parser.return_value.has_option.assert_any_call('main',
-                                                                  setting)
+    def test_with_empty_config_file_environment_variale():
+        with patch.dict(environ, {'CONFIG_FILE': ''}):
+            with pytest.raises(ConfigurationException,
+                               match=r'No configuration file provided.*'):
+                _ = Configuration()
+
+    @staticmethod
+    def test_with_if_config_file_cannot_be_read():
+        with patch.dict(environ, {'CONFIG_FILE': '/bad/path'}):
+            with pytest.raises(ConfigurationException,
+                               match=r'Failed to load configuration.*'):
+                _ = Configuration()
+
+    @staticmethod
+    def test_it_load_setting():
+        with patch.dict(environ, {'CONFIG_FILE': FIXTURE_CONFIG}):
+            configuration = Configuration()
+            assert configuration.puppetdb_host == 'somehost'
