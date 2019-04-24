649e37519ab463c123addf039b59578a38087c37
othalla <othalla.lf@gmail.com>
2019-04-24 16:57:23 +0200

    Add get_fact_path method to validate if path exist

--- puppetdb_exporter/puppetdb.py
+++ puppetdb_exporter/puppetdb.py
@@ -1,4 +1,4 @@
-from typing import Any, Iterator, Union
+from typing import Any, Dict, Iterator, Union
 
 from pypuppetdb import BaseAPI, connect
 from pypuppetdb.types import Node
@@ -18,3 +18,9 @@ def _get_puppetdb_connexion(configuration: Configuration) -> BaseAPI:
 def get_nodes(configuration: Configuration) -> Union[Any, Iterator[Node]]:
     database = _get_puppetdb_connexion(configuration)
     return database.nodes(with_status=True)
+
+
+def get_fact_path(path: str, configuration: Configuration) -> Dict[str, Any]:
+    database = _get_puppetdb_connexion(configuration)
+    formatted_fact_path = '"' + path.replace('.', '", "') + '"'
+    return database.fact_paths(query=f'["=", "path", [{formatted_fact_path}]]')
---
+++ tests/test_puppetdb.py
@@ -0,0 +1,11 @@
+from unittest.mock import MagicMock, patch
+
+from puppetdb_exporter.puppetdb import get_fact_path
+
+
+def test_get_fact_path():
+    with patch('puppetdb_exporter.puppetdb.connect') as mock_connect:
+        get_fact_path('fact.path',
+                      configuration=MagicMock(name='configurationn'))
+        mock_connect.return_value.fact_paths.assert_called_once_with(
+            query='["=", "path", ["fact", "path"]]')
