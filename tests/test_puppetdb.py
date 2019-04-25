e7ba568c9f2dc6a96b9e5d6581e7f7d0747728cf
othalla <othalla.lf@gmail.com>
2019-04-25 09:56:13 +0200

    Refactor puppetdb test names

--- tests/test_puppetdb.py
+++ tests/test_puppetdb.py
@@ -5,23 +5,25 @@ import pytest
 from puppetdb_exporter.puppetdb import FactNotFoundException, check_fact_path
 
 
-def test_get_fact_path_perform_correct_puppetdb_query():
-    with patch('puppetdb_exporter.puppetdb.connect') as mock_connect:
-        check_fact_path('fact.path',
-                        configuration=MagicMock(name='configurationn'))
-        mock_connect.return_value.fact_paths.assert_called_once_with(
-            query='["=", "path", ["fact", "path"]]')
-
-
-def test_get_fact_path_if_fact_not_found():
-    with patch('puppetdb_exporter.puppetdb.connect') as mock_connect:
-        mock_connect.return_value.fact_paths.return_value = []
-        with pytest.raises(FactNotFoundException):
+class TestChecKFactPath:
+    @staticmethod
+    def test_it_perform_correct_puppetdb_query():
+        with patch('puppetdb_exporter.puppetdb.connect') as mock_connect:
             check_fact_path('fact.path',
                             configuration=MagicMock(name='configurationn'))
+            mock_connect.return_value.fact_paths.assert_called_once_with(
+                query='["=", "path", ["fact", "path"]]')
 
+    @staticmethod
+    def test_if_fact_not_found():
+        with patch('puppetdb_exporter.puppetdb.connect') as mock_connect:
+            mock_connect.return_value.fact_paths.return_value = []
+            with pytest.raises(FactNotFoundException):
+                check_fact_path('fact.path',
+                                configuration=MagicMock(name='configurationn'))
 
-def test_get_fact_path_if_fact_found():
-    with patch('puppetdb_exporter.puppetdb.connect'):
-        check_fact_path('fact.path',
-                        configuration=MagicMock(name='configurationn'))
+    @staticmethod
+    def test_if_fact_found():
+        with patch('puppetdb_exporter.puppetdb.connect'):
+            check_fact_path('fact.path',
+                            configuration=MagicMock(name='configurationn'))
