0162806eb56a21b626c4e363acc4d7a7042e9145
othalla <othalla.lf@gmail.com>
2019-07-11 13:10:23 +0200

    Split facts & nodes metrics by environment

--- puppetdb_exporter/metrics.py
+++ puppetdb_exporter/metrics.py
@@ -5,10 +5,14 @@ from prometheus_client import Gauge
 from pypuppetdb.types import Node
 
 from puppetdb_exporter.config import Configuration
-from puppetdb_exporter.puppetdb import get_fact, get_nodes
+from puppetdb_exporter.puppetdb import get_fact, get_nodes, get_environments
 
-GAUGE_NODES = Gauge('puppetdb_nodes_registered', 'Description of gauge')
-GAUGE_STATUS = Gauge('puppetdb_nodes_status', 'desc', labelnames=['status'])
+GAUGE_NODES = Gauge('puppetdb_nodes_registered',
+                    'Description of gauge',
+                    labelnames=['environment'])
+GAUGE_STATUS = Gauge('puppetdb_nodes_status',
+                     'desc',
+                     labelnames=['status', 'environment'])
 GAUGE_FACTS: Dict[str, Gauge] = {}
 
 
@@ -17,38 +21,47 @@ class MetricsRender(Thread):
             self,
             configuration: Configuration,
             node_provider: Callable[
-                [Configuration],
+                [str, Configuration],
                 List[Node]] = get_nodes,
             fact_provider: Callable[
-                [str, Configuration],
-                List[Dict[str, Union[str, int]]]] = get_fact) -> None:
+                [str, str, Configuration],
+                List[Dict[str, Union[str, int]]]] = get_fact,
+            environments_provider: Callable[
+                [Configuration],
+                List[str]] = get_environments) -> None:
         Thread.__init__(self)
         self._configuration = configuration
         self._node_provider = node_provider
         self._fact_provider = fact_provider
+        self._environments_provider = environments_provider
 
     def run(self) -> None:
-        self._generate_nodes_metrics()
-        if self._configuration.fact_list:
-            self._generate_facts_metrics()
+        environments = self._environments_provider(self._configuration)
+        for environment in environments:
+            self._generate_nodes_metrics(environment)
+            if self._configuration.fact_list:
+                self._generate_facts_metrics(environment)
 
-    def _generate_facts_metrics(self) -> None:
+    def _generate_facts_metrics(self, environment: str) -> None:
         global GAUGE_FACTS  # pylint: disable=global-statement
         for fact in self._configuration.fact_list:
             formatted_fact = fact.replace('.', '_')
-            fact_results = self._fact_provider(fact, self._configuration)
+            fact_results = self._fact_provider(fact,
+                                               environment,
+                                               self._configuration)
             if formatted_fact not in GAUGE_FACTS:
                 GAUGE_FACTS[formatted_fact] = Gauge(
                     f'puppetdb_fact_{formatted_fact}',
                     'some gauge',
-                    labelnames=['value'])
+                    labelnames=['value', 'environment'])
             for fact_result in fact_results:
                 GAUGE_FACTS[formatted_fact].labels(
-                    value=fact_result['value']).set(fact_result['count'])
+                    value=fact_result['value'], environment=environment).set(
+                        fact_result['count'])
 
-    def _generate_nodes_metrics(self) -> None:
+    def _generate_nodes_metrics(self, environment: str) -> None:
         node_number = 0
-        nodes = self._node_provider(self._configuration)
+        nodes = self._node_provider(environment, self._configuration)
         status_values = {
             'changed': 0,
             'failed': 0,
@@ -63,6 +76,7 @@ class MetricsRender(Thread):
                 status_values[node.status] += 1
             except KeyError:
                 continue
-        GAUGE_NODES.set(node_number)
+        GAUGE_NODES.labels(environment=environment).set(node_number)
         for status, value in status_values.items():
-            GAUGE_STATUS.labels(status=status).set(value)
+            GAUGE_STATUS.labels(status=status,
+                                environment=environment).set(value)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -15,39 +15,70 @@ class TestMetricsRender:
     def test_it_set_the_nodes_registered_gauge_metric():
         get_nodes = MagicMock(name='get_nodes',
                               return_value=[NODE1, NODE2])
+        get_environments = MagicMock(name="get_environments",
+                                     return_value=['env1'])
         metrics = MetricsRender(configuration=MagicMock(name='Configuration',
                                                         fact_list=[]),
-                                node_provider=get_nodes)
+                                node_provider=get_nodes,
+                                environments_provider=get_environments)
         metrics.run()
-        assert REGISTRY.get_sample_value('puppetdb_nodes_registered') == 2
+        assert REGISTRY.get_sample_value('puppetdb_nodes_registered',
+                                         {'environment': 'env1'}) == 2
+
+    @staticmethod
+    def test_it_set_the_nodes_registered_gauge_metric_with_two__environments():
+        get_nodes = MagicMock(name='get_nodes',
+                              return_value=[NODE1, NODE2])
+        get_environments = MagicMock(name="get_environments",
+                                     return_value=['env1', 'env2'])
+        metrics = MetricsRender(configuration=MagicMock(name='Configuration',
+                                                        fact_list=[]),
+                                node_provider=get_nodes,
+                                environments_provider=get_environments)
+        metrics.run()
+        assert REGISTRY.get_sample_value('puppetdb_nodes_registered',
+                                         {'environment': 'env1'}) == 2
+        assert REGISTRY.get_sample_value('puppetdb_nodes_registered',
+                                         {'environment': 'env2'}) == 2
 
     @staticmethod
     def test_it_set_the_nodes_by_status_gauge_metric():
         get_nodes = MagicMock(name='get_nodes',
                               return_value=[NODE1, NODE2, NODE3])
+        get_environments = MagicMock(name="get_environments",
+                                     return_value=['env1'])
         metrics = MetricsRender(configuration=MagicMock(name='Configuration',
                                                         fact_list=[]),
-                                node_provider=get_nodes)
+                                node_provider=get_nodes,
+                                environments_provider=get_environments)
         metrics.run()
         assert REGISTRY.get_sample_value('puppetdb_nodes_status',
-                                         {'status': 'changed'}) == 2
+                                         {'status': 'changed',
+                                          'environment': 'env1'}) == 2
         assert REGISTRY.get_sample_value('puppetdb_nodes_status',
-                                         {'status': 'unchanged'}) == 1
+                                         {'status': 'unchanged',
+                                          'environment': 'env1'}) == 1
         assert REGISTRY.get_sample_value('puppetdb_nodes_status',
-                                         {'status': 'failed'}) == 0
+                                         {'status': 'failed',
+                                          'environment': 'env1'}) == 0
 
     @staticmethod
     def test_with_unknown_node_status():
         get_nodes = MagicMock(name='get_nodes',
                               return_value=[BAD_NODE])
+        get_environments = MagicMock(name="get_environments",
+                                     return_value=['env1'])
         metrics = MetricsRender(configuration=MagicMock(name='Configuration',
                                                         fact_list=[]),
-                                node_provider=get_nodes)
+                                node_provider=get_nodes,
+                                environments_provider=get_environments)
         metrics.run()
 
     @staticmethod
     def test_with_an_optional_fact_set_gauge_metric():
         get_nodes = MagicMock(name='get_nodes', return_value=[NODE1, NODE2])
+        get_environments = MagicMock(name="get_environments",
+                                     return_value=['env1'])
         get_fact = MagicMock(name='get_fact', side_effect=[
             [
                 {"value": "3.10.250", "count": 10},
@@ -62,24 +93,32 @@ class TestMetricsRender:
                                   fact_list=["kernelrelease", "os.name"])
         metrics = MetricsRender(configuration=configuration,
                                 node_provider=get_nodes,
-                                fact_provider=get_fact)
+                                fact_provider=get_fact,
+                                environments_provider=get_environments)
         metrics.run()
         assert REGISTRY.get_sample_value('puppetdb_fact_kernelrelease',
-                                         {'value': '3.10.250'}) == 10
+                                         {'value': '3.10.250',
+                                          'environment': 'env1'}) == 10
         assert REGISTRY.get_sample_value('puppetdb_fact_kernelrelease',
-                                         {'value': '3.10.290'}) == 15
+                                         {'value': '3.10.290',
+                                          'environment': 'env1'}) == 15
         assert REGISTRY.get_sample_value('puppetdb_fact_os_name',
-                                         {'value': 'foo'}) == 1
+                                         {'value': 'foo',
+                                          'environment': 'env1'}) == 1
         assert REGISTRY.get_sample_value('puppetdb_fact_os_name',
-                                         {'value': 'bar'}) == 5
+                                         {'value': 'bar',
+                                          'environment': 'env1'}) == 5
 
     @staticmethod
     def test_with_an_optional_fact_but_empty_puppetdb_response():
         get_nodes = MagicMock(name='get_nodes', return_value=[NODE1, NODE2])
+        get_environments = MagicMock(name="get_environments",
+                                     return_value=['env1'])
         get_fact = MagicMock(name='get_fact', return_value=[])
         configuration = MagicMock(name='configuration', fact_list=["badfact"])
         metrics = MetricsRender(configuration=configuration,
                                 node_provider=get_nodes,
-                                fact_provider=get_fact)
+                                fact_provider=get_fact,
+                                environments_provider=get_environments)
         metrics.run()
         assert REGISTRY.get_sample_value('puppetdb_fact_badfact') is None
